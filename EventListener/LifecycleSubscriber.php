a599a642322a21fa87fd36010d872b615f2cc8ad
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2013-03-05 15:24:21 +0100

    inital commit

---
+++ EventListener/LifecycleSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @package Newscoop\ExamplePluginBundle
+ * @author Paweł Mikołajczuk <pawel.mikolajczuk@sourcefabric.org>
+ * @copyright 2012 Sourcefabric o.p.s.
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+namespace Newscoop\ExamplePluginBundle\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Newscoop\EventDispatcher\Events\GenericEvent;
+
+/**
+ * Event lifecycle management
+ */
+class LifecycleSubscriber implements EventSubscriberInterface
+{
+    public function install(GenericEvent $event)
+    {
+        // do something on install
+    }
+
+    public function update(GenericEvent $event)
+    {
+        // do something on update
+    }
+
+    public function remove(GenericEvent $event)
+    {
+        //do something on remove
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            'plugin.install' => array('install', 1),
+            'plugin.update' => array('update', 1),
+            'plugin.remove' => array('remove', 1),
+        );
+    }
+}
